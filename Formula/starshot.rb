b82bac30e9e3f4ff8aa0fecd04be686d40f827c2
James Seddon <js@rockstarsunlimited.co>
2026-08-01 10:46:24 +0100

    Fix formula component ordering

--- Formula/starshot.rb
+++ Formula/starshot.rb
@@ -8,8 +8,8 @@ class Starshot < Formula
   url "https://github.com/rockstarsunlimited/starshot/releases/download/v0.2.0/starshot-macos.zip"
   version "0.2.0"
   sha256 "9e1ca7773163ffa417abbccb05649849572b2cce7defc74e2f37a8119325cb58"
-  revision 1
   license "MIT"
+  revision 1
 
   depends_on "bun"
   depends_on :macos
